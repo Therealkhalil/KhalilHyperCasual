80ac190fbd882f4416fbb300c4edfb45006d951e
Therealkhalil <khalil.chabir96@gmail.com>
2023-01-10 18:58:11 +0100

    Movement player functions

---
+++ Hypercasual mertay8/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] private float limitvalue;
+    [SerializeField] private Transform TransformPlayer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            MovePlayer();
+        }
+
+    }
+
+
+    private void MovePlayer()
+    {
+        //Calculate X position and modify it
+        float halfScreen = Screen.width / 2;
+        float xPos = (Input.mousePosition.x - halfScreen) / halfScreen;
+        float finalPosx = Mathf.Clamp(xPos * limitvalue,-limitvalue,limitvalue);
+
+        TransformPlayer.localPosition = new Vector3(finalPosx, 1, 0); 
+
+        
+    }
+}
