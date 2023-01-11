8af3a6405790e6b8f3cf4d77e0d8ef2a98c5eb5f
Therealkhalil <khalil.chabir96@gmail.com>
2023-01-11 10:51:44 +0100

    Fix Character Teleportation (smooth the movement)

--- Hypercasual mertay8/Assets/Scripts/PlayerMovement.cs
+++ Hypercasual mertay8/Assets/Scripts/PlayerMovement.cs
@@ -7,12 +7,15 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField] private float limitvalue;
     [SerializeField] private Transform TransformPlayer;
+    [SerializeField] private float speedHorizontal;
+    private Touch touchPos;
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButton(0))
+        if(Input.touchCount > 0)
         {
+            touchPos = Input.GetTouch(0);
             MovePlayer();
         }
 
@@ -23,8 +26,12 @@ public class PlayerMovement : MonoBehaviour
     {
         //Calculate X position and modify it
         float halfScreen = Screen.width / 2;
-        float xPos = (Input.mousePosition.x - halfScreen) / halfScreen;
-        float finalPosx = Mathf.Clamp(xPos * limitvalue,-limitvalue,limitvalue);
-        TransformPlayer.localPosition = new Vector3(finalPosx, 0, 0);    
+        float xPos = (touchPos.position.x - halfScreen) / halfScreen;
+        float finalPosx = Mathf.Clamp(xPos * limitvalue, -limitvalue, limitvalue);
+        float moveX = finalPosx - TransformPlayer.localPosition.x;
+        TransformPlayer.localPosition += new Vector3(moveX * speedHorizontal * Time.deltaTime, 0, 0);
     }
 }
+
+
+//float xPos = (Input.mousePosition.x - halfScreen) / halfScreen;
