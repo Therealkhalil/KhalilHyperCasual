cdbee8291fe5ad89b67dd5f2dd69563252641788
Therealkhalil <khalil.chabir96@gmail.com>
2023-01-14 00:19:47 +0100

    add camera movement when you win

--- Hypercasual mertay8/Assets/Script/CharCheck.cs
+++ Hypercasual mertay8/Assets/Script/CharCheck.cs
@@ -1,11 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class CharCheck : MonoBehaviour
 {
+    [SerializeField] private GameObject camera; 
     public bool is_dead = false,is_win = false;
     [SerializeField] private Animator chanim;
+    private Transform target;
+
+    private void Update()
+    {
+        if(is_win)
+        {
+            camera.transform.localEulerAngles = new Vector3(13.5f, 169f, 1.713f);
+            camera.transform.position = Vector3.Lerp(camera.transform.position, new Vector3(0f, 2f, 80.01998f), Time.deltaTime);
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
